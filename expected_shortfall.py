038f1813fe1138b364cb14b9c2f7c5a9ae60fe02
Pablo Sánchez <32568877+psanchezc23@users.noreply.github.com>
2020-03-01 18:55:06 +0100

    ENH: Improving docstring and code

--- expected_shortfall.py
+++ expected_shortfall.py
@@ -1,25 +1,20 @@
-def expected_shortfall(returns, confidence_level=.05):
+def expected_shortfall(assets_returns, confidence_level=.05):
 	"""
 	It calculates the Expected Shortfall (ES) of some time series. It represents 
 	the average loss according to the Value at Risk.
 	
-	Parameters
-	----------
-	returns : pandas.DataFrame
-		Returns of each time serie. It could be daily, weekly, monthly, ...
-		
-	confidence_level : int
-		Confidence level. 5% by default.
-			
-	Returns
-	-------
-	es : pandas.Series
-		Expected Shortfall for each time series.
+	:param pandas.DataFrame assets_returns: Returns of each asset. It could be daily, weekly, monthly, ...
+	:param int confidence_level: Confidence level. 5% by default.
+	
+	:return expected_shortfall: Expected Shortfall for each time series.
+	:rtype: pandas.Series
 	
 	"""
 	
 	# Calculating VaR
-	var = value_at_risk(returns, confidence_level)
+	var = value_at_risk(assets_returns, confidence_level)
 	
 	# ES is the average of the worst losses (under var)
-	return returns[returns.lt(var, axis=1)].mean()
+	expected_shortfall = assets_returns[assets_returns.lt(var, axis=1)].mean()
+	
+	return expected_shortfall
